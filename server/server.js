84f208ed503133135a4e1b65d794bbe81d893df1
mayanme <mayanm152@gmail.com>
2021-03-30 14:26:46 +0200

    trying to fix error H21 backend connection refused 3

--- server/server.js
+++ server/server.js
@@ -7,8 +7,6 @@ const fs = require('fs');
 const path = require('path');
 // var cookies = require('cookie-parser');
 
-
-
 const config = {
     // mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/maymen',
     mongoURL: "mongodb+srv://maymen:Mm23071994@mayancluster.zuclv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -27,10 +25,7 @@ if (process.env.NODE_ENV !== 'test') {
   });
 }
 
-
-
 const app = express();
-
 
 //body parser for json. must be done before API routes
 app.use(express.urlencoded({extended:true})); //handle body requests
@@ -39,13 +34,21 @@ app.use(express.json());
 // app.use(express.static(path.join(__dirname, 'public')));
 // app.use(cookies());
 
-
-
 // Add backend api routes
 fs.readdirSync(__dirname + '/api').forEach((file) => {
   console.log(`./api/${file.substr(0, file.indexOf('.'))}`);
   require(`./api/${file.substr(0, file.indexOf('.'))}`)(app);
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
 
 app.use(function (err, msg, req, res, next) {
   console.error(err.stack);
